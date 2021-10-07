049cbc43079d1b2e5bf645e887e4040145fb1429
zopaj63 <zoran.pajnic@gmail.com>
2021-10-08 00:40:53 +0200

    add Link

--- src/pages/SignInPage/index.js
+++ src/pages/SignInPage/index.js
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function SignInPage() {
-    return <h1>Sign in</h1>;
+    return (
+        <div>
+            <h1>Sign in</h1>
+            <br />
+            <p>... or go to <Link to="chat">chat</Link> :)</p>
+        </div>
+    );
 }
