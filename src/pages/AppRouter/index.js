18b2817b067ac44d8a5dc03848fdd6e6cbd47b89
zopaj63 <zoran.pajnic@gmail.com>
2021-10-11 19:29:47 +0200

    add NasaPage

--- src/pages/AppRouter/index.js
+++ src/pages/AppRouter/index.js
@@ -1,6 +1,7 @@
 import { Route, Switch, NavLink, Redirect } from "react-router-dom";
 import SignInPage from "../SignInPage";
 import ChatPage from "../ChatPage";
+import NasaPage from "../NasaPage";
 
 export default function AppRouter() {
     return (
@@ -8,10 +9,12 @@ export default function AppRouter() {
             <ul>
                 <li><NavLink exact to="/">Index</NavLink></li>
                 <li><NavLink to="/chat">Chat</NavLink></li>
+                <li><NavLink to="/nasa">NASA</NavLink></li>
             </ul>
             <Switch>
                 <Route exact path="/" component={SignInPage} />
                 <Route path="/chat" component={ChatPage} />
+                <Route path="/nasa" component={NasaPage} />
                 <Route path="*">
                     <Redirect to="/" />
                 </Route>
